43413c62caf4566071cafe195e777831c3858ffe
Martin Brandl <mjibrandl@gmail.com>
2019-08-08 08:49:39 +0200

    Added middleware

---
+++ foo.api/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace foo.api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetAsync() => Ok("Hello, World!");
+    }
+}
